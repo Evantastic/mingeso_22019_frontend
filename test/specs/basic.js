63f5043ce20537494ec795bc2954b5fea6eba422
Nicole Martín Ramos <nicole.martin@usach.cl>
2019-09-25 03:34:25 -0300

    Pruebas de aceptación

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -7,3 +7,23 @@ describe('webdriver.io page', () => {
         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
     })
 })
+
+describe('webdriver.io page', () => {
+    it('should switch to another window', () => {
+        // open url
+        browser.url('https://google.com')
+        // create new window
+        browser.newWindow('https://webdriver.io')
+
+        // switch back via url match
+        browser.switchWindow('google.com')
+
+    })
+})
+
+describe('webdriver.io page', () => {
+    // navigate to a new URL
+    browser.url('https://webdriver.io');
+    // receive url
+    console.log(browser.getUrl());
+})
